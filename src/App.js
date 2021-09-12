fee54db63de4c1b0b76e35dc1d14ecddd5d4dee0
Sagar Awale <sagarawale09@gmail.com>
2021-09-12 13:53:16 +0530

    changed the text

--- src/App.js
+++ src/App.js
@@ -14,7 +14,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          React is good !!!
         </a>
       </header>
     </div>
